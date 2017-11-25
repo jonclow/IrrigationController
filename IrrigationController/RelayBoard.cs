fc82351aa2f55e340e884d8e80336be356ba9405
Jonathan Clow <jonclow@redmercury.co.nz>
2017-11-25 21:51:16 +1300

    Fix display res. Refactor timers and events.

---
+++ IrrigationController/RelayBoard.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+
+namespace IrrigationController
+{
+    public sealed class RelayBoard
+    {
+        // Relay Control Pins
+        private const int HERB_PIN = 6;
+        private const int ORCHARD_PIN = 5;
+        private const int TUNNEL_HOUSE_PIN = 13;
+        private const int GARDEN_PIN = 26;
+
+        // Control Flags
+        private bool available = false;
+        private bool enable = false;
+
+        /// <summary>
+        /// Relay switch control pins for the relevant areas solenoid valve
+        /// </summary>
+        /// <remarks>
+        /// These object will be created in InitAsync(). The set method will
+        /// be marked private, because the object itself will not change, only
+        /// the value it drives to the pin.
+        /// </remarks>
+        public GpioPin HerbPin { get; private set; }
+        public GpioPin OrchardPin { get; private set; }
+        public GpioPin TunnelHousePin { get; private set; }
+        public GpioPin GardenPin { get; private set; }
+
+        public void Begin()
+        {
+            /*
+                 * Acquire the GPIO controller
+                 * MSDN GPIO Reference: https://msdn.microsoft.com/en-us/library/windows/apps/windows.devices.gpio.aspx
+                 * 
+                 * Get the default GpioController
+                 */
+            GpioController gpio = GpioController.GetDefault();
+
+            /*
+             * Test to see if the GPIO controller is available.
+             *
+             * If the GPIO controller is not available, this is
+             * a good indicator the app has been deployed to a
+             * computing environment that is not capable of
+             * controlling the relay switch. Therefore we
+             * will disable the functionality to
+             * handle the failure case gracefully. This allows
+             * the invoking application to remain deployable
+             * across the Universal Windows Platform.
+             */
+            if (null == gpio)
+            {
+                available = false;
+                enable = false;
+                return;
+            }
+
+            /*
+                 * Initialize the area solenoid drives and set to "off"
+                 *
+                 * Instantiate the relevant pin object
+                 * Write the GPIO pin value of low on the pin
+                 * Set the GPIO pin drive mode to output
+                 */
+            HerbPin = gpio.OpenPin(HERB_PIN, GpioSharingMode.Exclusive);
+            //HerbPin.Write(GpioPinValue.Low);
+            HerbPin.SetDriveMode(GpioPinDriveMode.Output);
+
+            OrchardPin = gpio.OpenPin(ORCHARD_PIN, GpioSharingMode.Exclusive);
+            //OrchardPin.Write(GpioPinValue.Low);
+            OrchardPin.SetDriveMode(GpioPinDriveMode.Output);
+
+            TunnelHousePin = gpio.OpenPin(TUNNEL_HOUSE_PIN, GpioSharingMode.Exclusive);
+            //TunnelHousePin.Write(GpioPinValue.Low);
+            TunnelHousePin.SetDriveMode(GpioPinDriveMode.Output);
+
+            GardenPin = gpio.OpenPin(GARDEN_PIN, GpioSharingMode.Exclusive);
+            //GardenPin.Write(GpioPinValue.Low);
+            GardenPin.SetDriveMode(GpioPinDriveMode.Output);
+
+            available = true;
+            enable = true;
+
+        }
+
+        public bool SwitchRelay(GpioPin pinIdent)
+        {
+            if(available)
+            {
+                try
+                {
+                    if(pinIdent.Read() == GpioPinValue.Low)
+                    {
+                        pinIdent.Write(GpioPinValue.High);
+                        return false;
+                        //return ("Relay is off!");
+                    }
+                    else
+                    {
+                        pinIdent.Write(GpioPinValue.Low);
+                        return true;
+                        //return ("Relay is on!");
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                    //return (ex.Message.ToString());
+                }
+            }
+
+            return false;
+            //return ("Relay NOT available - restart");
+        }
+    }
+}
--- IrrigationController/ValveControl.xaml.cs
+++ IrrigationController/ValveControl.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using System.Threading;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +13,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.ApplicationModel.Background;
+using Windows.Devices.Gpio;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,6 +25,24 @@ namespace IrrigationController
     /// </summary>
     public sealed partial class ValveControl : Page
     {
+        private DispatcherTimer irrigationTimer;
+        private DispatcherTimer gardenTimer;
+        private DispatcherTimer herbTimer;
+        private DispatcherTimer tunnelHouseTimer;
+        private DispatcherTimer orchardTimer;
+        private TimeSpan duration;
+        DateTimeOffset time;
+        DateTimeOffset startTime;
+        DateTimeOffset lastTime;
+
+
+        private SolidColorBrush redBrush = new SolidColorBrush(Windows.UI.Colors.Red);
+        private SolidColorBrush greenBrush = new SolidColorBrush(Windows.UI.Colors.Green);
+
+        public Dictionary<string, bool> valveState = new Dictionary<string, bool>();
+
+        private RelayBoard RelayBoard = new RelayBoard();
+
         private int _irrigateDuration;
 
         public int IrrigateDuration { get => _irrigateDuration; set => _irrigateDuration = value; }
@@ -29,43 +50,160 @@ namespace IrrigationController
         public ValveControl()
         {
             this.InitializeComponent();
+
+            RelayBoard.Begin();
+           
+            valveState.Add("Herbs", false);
+            valveState.Add("Orchard", false);
+            valveState.Add("TunnelHouse", false);
+            valveState.Add("Garden", false);
         }
 
         private void HerbsButton_Click(object sender, RoutedEventArgs e)
         {
+            valveState["Herbs"] = RelayBoard.SwitchRelay(RelayBoard.HerbPin);
+            Herbs.Background = SetButtonColour("Herbs");
 
+            makeTheSwitches("Herbs", herbTimer, RelayBoard.HerbPin, Herbs);
+
+            /*
+            herbTimer = new DispatcherTimer();
+            herbTimer.Interval = new TimeSpan(0, IrrigateDuration, 0);
+
+            herbTimer.Start();
+
+            herbTimer.Tick += (s, ev) =>
+            {
+                herbTimer.Stop();
+                valveState["Herbs"] = RelayBoard.SwitchRelay(RelayBoard.HerbPin);
+                Herbs.Background = SetButtonColour("Herbs");
+            };
+            */
         }
 
         private void OrchardButton_Click(object sender, RoutedEventArgs e)
         {
+            valveState["Orchard"] = RelayBoard.SwitchRelay(RelayBoard.OrchardPin);
+            Orchard.Background = SetButtonColour("Orchard");
 
+            makeTheSwitches("Orchard", orchardTimer, RelayBoard.OrchardPin, Orchard);
+
+            /*
+            orchardTimer = new DispatcherTimer();
+            orchardTimer.Interval = new TimeSpan(0, IrrigateDuration, 0);
+
+            orchardTimer.Start();
+
+            orchardTimer.Tick += (s, ev) =>
+            {
+                orchardTimer.Stop();
+                valveState["Orchard"] = RelayBoard.SwitchRelay(RelayBoard.OrchardPin);
+                Herbs.Background = SetButtonColour("Orchard");
+            };
+            */
         }
 
         private void TunnelHouseButton_Click(object sender, RoutedEventArgs e)
         {
+            valveState["TunnelHouse"] = RelayBoard.SwitchRelay(RelayBoard.TunnelHousePin);
+            TunnelHouse.Background = SetButtonColour("TunnelHouse");
 
+            makeTheSwitches("TunnelHouse", tunnelHouseTimer, RelayBoard.TunnelHousePin, TunnelHouse);
+
+            /*
+            tunnelHouseTimer = new DispatcherTimer();
+            tunnelHouseTimer.Interval = new TimeSpan(0, IrrigateDuration, 0);
+
+            tunnelHouseTimer.Start();
+
+            tunnelHouseTimer.Tick += (s, ev) =>
+            {
+                tunnelHouseTimer.Stop();
+                valveState["TunnelHouse"] = RelayBoard.SwitchRelay(RelayBoard.TunnelHousePin);
+                Herbs.Background = SetButtonColour("TunnelHouse");
+            };
+            */
         }
 
         private void GardenButton_Click(object sender, RoutedEventArgs e)
         {
+            valveState["Garden"] = RelayBoard.SwitchRelay(RelayBoard.GardenPin);
+            Garden.Background = SetButtonColour("Garden");
 
+            makeTheSwitches("Garden", gardenTimer, RelayBoard.GardenPin, Garden);
+
+            // Check if the valve is open - if so, start timer to then close it.
+            /*
+            if (valveState["Garden"])
+            {
+                gardenTimer = new DispatcherTimer();
+                gardenTimer.Interval = new TimeSpan(0, IrrigateDuration, 0);
+
+                gardenTimer.Start();
+
+                gardenTimer.Tick += (s, ev) =>
+                {
+                    gardenTimer.Stop();
+                    valveState["Garden"] = RelayBoard.SwitchRelay(RelayBoard.GardenPin);
+                    Herbs.Background = SetButtonColour("Garden");
+                };
+            }
+            else
+            {
+                // We just closed the valve, but there may be a timer running if we closed it before the duration finished.
+                if (gardenTimer.IsEnabled)
+                {
+                    gardenTimer.Stop();
+                }
+            }
+            */
+            
+        }
+
+        private void makeTheSwitches(string whichArea, DispatcherTimer timer, GpioPin pin, Button button)
+        {
+            // Check if the valve is open - if so, start timer to then close it.
+            if (valveState[whichArea])
+            {
+                timer = new DispatcherTimer();
+                timer.Interval = new TimeSpan(0, IrrigateDuration, 0);
+
+                timer.Start();
+
+                timer.Tick += (s, ev) =>
+                {
+                    timer.Stop();
+                    valveState[whichArea] = RelayBoard.SwitchRelay(pin);
+                    button.Background = SetButtonColour(whichArea);
+                };
+            }
+            else
+            {
+                // We just closed the valve, but there may be a timer running if we closed it before the duration finished.
+                if (timer.IsEnabled)
+                {
+                    timer.Stop();
+                }
+            }
         }
 
         private void durationSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
         {
-            string msg = String.Format("Current value: {0}", e.NewValue);
-            this.durationValue.Text = msg;
+            //string msg = String.Format("Current value: {0}", e.NewValue);
+            //durationValue.Text = msg;
             IrrigateDuration = (int) e.NewValue;
         }
 
-        private void IrrigationStartButton_Click(object sender, RoutedEventArgs e)
+        private SolidColorBrush SetButtonColour(string button)
         {
-
-        }
-
-        private void IrrigationStopButton_Click(object sender, RoutedEventArgs e)
-        {
-
+            if (valveState[button])
+            {
+                return greenBrush;
+            }
+            else
+            {
+                return redBrush;
+            }
         }
     }
 }
